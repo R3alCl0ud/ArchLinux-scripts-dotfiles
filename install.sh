ba7d7e2237322478f9345ca0e8ee4485f1067e51
Neptune <ultra.neppers@gmail.com>
2021-07-08 15:56:49 -0700

    oops v2

--- install.sh
+++ install.sh
@@ -1,40 +1,3 @@
-# Mounts my btrfs subvolumes
-function mount_subvols() {
-  echo mounting root subvolume
-  mount -o noatime,compress=lzo,space_cache,discard=async,subvol=@ $install_drive /mnt/
-  echo creating subvolume directories if not already existing
-  mkdir -p /mnt/{boot,home}
-  echo mounting home subvolume
-  mount -o noatime,compress=lzo,space_cache,discard=async,subvol=@home $install_drive /mnt/home
-  # mount -o noatime,compress=lzo,space_cache,discard=async,subvol=@.snapshots /dev/nvme0n1p3 /mnt/.snapshots
-  echo "Enter boot partition:"
-  read -p "" efi_sp
-  echo mouting boot parition $efi_sp
-  mount $efi_sp /mnt/boot
-
-  echo "Intel or AMD?"
-  read -p "" cpu_platform
-
-  if ["$cpu_platform" == "amd"]; then
-    install_amd
-    elif ["$cpu_platform" == "intel"]; then
-      install_intel
-    fi
-}
-
-function create_subvols() {
-  
-  echo mounting btrfs
-  echo $install_drive
-  mount -t btrfs $install_drive /mnt
-  btrfs su cr /mnt/@
-  btrfs su cr /mnt/@home
-
-  umount /mnt
-
-  mount_subvols $install_drive
-}
-
 function install_amd() {
 
   pacstrap /mnt base linux-zen linux-zen-headers vim amd-ucode btrfs-progs openssh grub
@@ -56,6 +19,45 @@ function install_intel() {
 
   arch-chroot /mnt
 
+}
+
+
+
+# Mounts my btrfs subvolumes
+function mount_subvols() {
+  echo mounting root subvolume
+  mount -o noatime,compress=lzo,space_cache,discard=async,subvol=@ $install_drive /mnt/
+  echo creating subvolume directories if not already existing
+  mkdir -p /mnt/{boot,home}
+  echo mounting home subvolume
+  mount -o noatime,compress=lzo,space_cache,discard=async,subvol=@home $install_drive /mnt/home
+  # mount -o noatime,compress=lzo,space_cache,discard=async,subvol=@.snapshots /dev/nvme0n1p3 /mnt/.snapshots
+  echo "Enter boot partition:"
+  read -p "" efi_sp
+  echo mouting boot parition $efi_sp
+  mount $efi_sp /mnt/boot
+
+  echo "Intel or AMD?"
+  read -p "" cpu_platform
+
+  if ["$cpu_platform" = "amd"]; then
+    install_amd
+    elif ["$cpu_platform" = "intel"]; then
+      install_intel
+    fi
+}
+
+function create_subvols() {
+  
+  echo mounting btrfs
+  echo $install_drive
+  mount -t btrfs $install_drive /mnt
+  btrfs su cr /mnt/@
+  btrfs su cr /mnt/@home
+
+  umount /mnt
+
+  mount_subvols $install_drive
 }
 
 function start() {
